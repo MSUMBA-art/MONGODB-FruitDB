f3b57619ef296e99eb7ff753590a7ed7b0d29d08
ALEXANDER <msumba83@gmail.com>
2022-01-31 14:40:49 +0200

    mongodb CRUD finished

--- app.js
+++ app.js
@@ -8,45 +8,70 @@ async function main() {
 }
 
 const fruitSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
+    name: {
+      type: String, 
+      //required: [true, "PLEASE ADD NAME"], 
+    },
+    rating: {
+      type: Number,
+      min: 1, 
+      max: 10
+    },
     review: String
 });
 
 const fruit = mongoose.model('fruit', fruitSchema);
 
 const fruits = new fruit({
-    name: 'Apple',
-    rating: 5,
+    name: 'Paw paw',
+    rating: 10,
     review: 'Deliscious'
 });
 
-// fruits.save();
+fruits.save();
 
 const personSchema = new mongoose.Schema({
   name: String,
-  age: Number
+  age: Number,
+  favouriteFruit: fruitSchema 
 })
 
 const Person = mongoose.model("Person", personSchema);
-const person = new Person ({
-  name: "ALEXANDER",
-  age: 40
-});
+const mango = new fruit({
+  name: "mango",
+  rating: 9,
+  review: "great staff"
+})
+
+Person.updateOne({name:"ALEXANER"}, {favouriteFruit: "mango"}, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("AJ LIKES MANGO");
+  }
+} );
+
+mango.save()
+
+// const person = new Person ({
+//   name: "ANDER",
+//   age: 30,
+//   favouriteFruit: "pineapple"
+// });
 
 //person.save();
 
-const peaches = new fruit({
-  name: "peaches",
-  score: 6,
-  review: "The best"
-});
+// const peaches = new fruit({
+//   name: "peaches",
+//   score: 6,
+//   review: "The best"
+// });
 
-const mango = new fruit({
-  name: "mango",
-  score: 2,
-  review: "The best ever"
-});
+// const mango = new fruit({
+//   name: "mango",
+//   score: 2,
+//   review: "The best ever"
+// });
 
 // const grapes = new fruit({
 //   name: "grapes",
@@ -72,6 +97,67 @@ fruit.find( function(err, fruits) {
   });
   }
 });
+
+// fruit.updateOne( {_id: "61f7b79cf3a5ac5032af44e7"}, {name: "potato"}, function (err) {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//    console.log("Successfully added an item") ;
+//   }
+// } );
+
+// Person.deleteMany( {name: "ALEXANER"}, function (err) {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//    console.log("Successfully deleted an item") ;
+//   }
+// } );
+
+// fruit.deleteOne( {name: "Paw paw"}, function (err) {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//    console.log("Successfully deleted an item") ;
+//   }
+// } );
+
+// fruit.deleteOne( {name: "Paw paw"}, function (err) {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//    console.log("Successfully deleted an item") ;
+//   }
+// } );
+
+// fruit.deleteMany({name: "pineapple"}, function (err, res){
+// })
+
+// Person.deleteMany( {name: "ALEXANDER"}, function (err) {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//    console.log("Successfully deleted an AJ") ;
+//   }
+// } );
+
+const bookSchema = new mongoose.Schema({
+   name: String,
+   rating: Number,
+   review: String
+ })
+const Book = mongoose.model("Book", bookSchema);
+const book = new Book ({
+  name: "Bible",
+  rating: 5,
+  review: "wonderful book"
+});
+
+//book.save();
+
+
+
+
 
 
 
